c6f214cdd1f4da541fedc6aa06f9bd9d9d8cbc90
Narek Torosyan <narek.email@gmail.com>
2021-11-07 21:20:53 +0400

    Some stuff in AppSearch

--- src/com/android/messaging/ui/AppSearchActivity.java
+++ src/com/android/messaging/ui/AppSearchActivity.java
@@ -1,8 +1,20 @@
 package com.android.messaging.ui;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.appsearch.SearchResults;
+
 import com.android.messaging.R;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import android.app.appsearch.SearchSpec;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class AppSearchActivity extends AppCompatActivity {
 
@@ -10,5 +22,8 @@ public class AppSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_search);
+    }
+
+    public void search(View view) {
     }
 }
